68ead450f809249213a1767cfdf89010337e1c72
Kent Gustavsson <nedo80@gmail.com>
2012-10-16 00:22:58 +0200

    Dummy payment handler, used for testing

---
+++ dommyPaymentHandler/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using kShop;
+
+namespace dummyPaymentHandler
+{
+    public class Class1 : kShop.PaymentProvider
+    {
+        public virtual string getName()
+        {
+            return "DUMMY";
+        }
+
+        public virtual bool list()
+        {
+            return true;
+        }
+
+        public string createPaymentRequest(Cart cart)
+        {
+            return "Payment Request";
+        }
+    }
+}
---
+++ dommyPaymentHandler/SecondPaymentProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using kShop;
+
+namespace dummyPaymentHandler
+{
+    /// <summary>
+    /// Testing with inharitance, using new doesn't work for some reason
+    /// </summary>
+    class SecondPaymentProvider : Class1
+    {
+        public override string getName()
+        {
+            return "DUMMY3";
+        }
+
+        public override bool list()
+        {
+            return true;
+        }
+
+    }
+}
