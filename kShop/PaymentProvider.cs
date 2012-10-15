82f7cf9e779c3f8c7e5ff09c6e8d61d5fcd2869c
Kent Gustavsson <nedo80@gmail.com>
2012-10-16 00:22:01 +0200

    Added payment provider interface

---
+++ kShop/PaymentProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kShop
+{
+    public interface PaymentProvider
+    {
+        string getName();
+        string createPaymentRequest(Cart cart);
+        bool list();
+    }
+}
