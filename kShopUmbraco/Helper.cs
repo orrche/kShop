781cdc0820269ca869da26fdf5d397a8224432f1
Kent Gustavsson <nedo80@gmail.com>
2012-11-22 16:25:36 +0100

    Added class comment

--- kShopUmbraco/Helper.cs
+++ kShopUmbraco/Helper.cs
@@ -8,9 +8,11 @@ using kShop;
 using umbraco.MacroEngines;
 using System.Web;
 
-
 namespace kShopUmbraco
 {
+    /// <summary>
+    ///  These probably should be possible to overload somehow, just not sure how....
+    /// </summary>
     public class Helper
     {
         static Catalog catalog = null;
